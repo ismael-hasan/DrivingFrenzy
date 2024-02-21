a70ffdb7e9446637b80f8963b74aac1c2ab942a2
Ismael <ismael.hasan.romero@gmail.com>
2024-02-21 09:31:53 +0100

    Added sections with handicap. Drivers can get disqualified if they go too fast

--- src/drivingFrenzy/main/ControlCenter.java
+++ src/drivingFrenzy/main/ControlCenter.java
@@ -2,13 +2,11 @@ package drivingFrenzy.main;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Map.Entry;
 import java.util.Random;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
 import drivingFrenzy.race.Section;
 import drivingFrenzy.race.StandardIndoorSection;
+import drivingFrenzy.race.StandardOutdoorSection;
 import drivingFrenzy.race.Track;
 import drivingFrenzy.vehicles.Kart;
 import drivingFrenzy.vehicles.Scooter;
@@ -77,7 +75,7 @@ public class ControlCenter {
 	/**
 	 * @throws IOException
 	 * 
-	 * Creates a race with 5 sections and 3 scooters
+	 *                     Creates a race with 5 sections and 3 scooters
 	 */
 	private static void defaultRace() throws IOException {
 		// We have a track with 5 sections of 1000, 2000, 3000, 2000 and 1000 meters of
@@ -96,7 +94,7 @@ public class ControlCenter {
 		Track track = new Track(sections);
 		start(track, vehicles);
 	}
-	
+
 	private static void kartsRace() throws IOException {
 		// We have a track with 5 sections of 1000, 2000, 3000, 2000 and 1000 meters of
 		// length; you can choose the max speed.
@@ -132,14 +130,44 @@ public class ControlCenter {
 		vehicles[0] = new Kart(0, "Carlos", 0, 50, 40, 100, "kart rojo");
 		vehicles[1] = new Kart(1, "Carlos Jr", 0, 30, 25, 80, "kart rojo");
 		vehicles[2] = new Kart(2, "Fernando", 0, 60, 55, 110, "kart rojo");
-		vehicles[3] = new Scooter(0, "Valentino", 0, 70, "Ariic Gemma");
-		vehicles[4] = new Scooter(1, "Marc", 0, 87, "Daelim Besbi");
-		vehicles[5] = new Scooter(2, "Dani", 0, 95, "Honda Forza");
+		vehicles[3] = new Scooter(3, "Valentino", 0, 70, "Ariic Gemma");
+		vehicles[4] = new Scooter(4, "Marc", 0, 87, "Daelim Besbi");
+		vehicles[5] = new Scooter(5, "Dani", 0, 95, "Honda Forza");
 
 		Track track = new Track(sections);
 		start(track, vehicles);
 	}
 
+	/**
+	 * @throws IOException It combines indoor and outdoor.
+	 */
+	private static void kartsAndScootersMixedRace() throws IOException {
+		// We have a track with 5 sections of 1000, 2000, 3000, 2000 and 1000 meters of
+		// length; you can choose the max speed.
+		Section[] sections = new Section[10];
+
+		sections[0] = new StandardIndoorSection(100, "salida desde el pabellón", 20);
+		sections[1] = new StandardIndoorSection(1000, "recta inicial", 100);
+		sections[2] = new StandardIndoorSection(1000, "primera curva de salida al exterior", 50);
+		sections[3] = new StandardOutdoorSection(1000, "recta exterior muy soleada", 120, 1.1);
+		sections[4] = new StandardOutdoorSection(500, "segunda curva muy cerrada, todavía soleada", 30, 1.1);
+		sections[5] = new StandardOutdoorSection(2000, "recta de vuelta al pabellón", 100, 1.1);
+		sections[6] = new StandardOutdoorSection(500, "curva de entrada al pabellón, alguien ha derramado aceite", 50,
+				0.8);
+		sections[7] = new StandardIndoorSection(1000, "recta media", 120);
+		sections[8] = new StandardIndoorSection(1000, "tercera curva muy cerrada", 30);
+		sections[9] = new StandardIndoorSection(1000, "recta final", 100);
+		Vehicle[] vehicles = new Vehicle[6];
+		vehicles[0] = new Kart(0, "Carlos", 0, 50, 40, 100, "kart rojo");
+		vehicles[1] = new Kart(1, "Carlos Jr", 0, 30, 25, 80, "kart rojo");
+		vehicles[2] = new Kart(2, "Fernando", 0, 60, 55, 110, "kart rojo");
+		vehicles[3] = new Scooter(3, "Valentino", 0, 70, "Ariic Gemma");
+		vehicles[4] = new Scooter(4, "Marc", 0, 87, "Daelim Besbi");
+		vehicles[5] = new Scooter(5, "Dani", 0, 95, "Honda Forza");
+
+		Track track = new Track(sections);
+		start(track, vehicles);
+	}
 
 	/**
 	 * This method receives a track and a list of cars and it starts a race, showing
@@ -153,6 +181,10 @@ public class ControlCenter {
 		// We will track the total time per vehicle in an array matching positions.
 		// However, this should be done differently, with proper Java Objects.
 		double[] times = new double[vehicles.length];
+		// We will store, for each vehicle, in which section it was disqualified. Null
+		// if it was not disqualified.
+		Section[] disqualifiedVehicles = new Section[vehicles.length];
+		Arrays.fill(disqualifiedVehicles, null);
 
 		// NOW WE START THE RACE!!!! We have to get the times for each vehicle per
 		// section, and then the total time.
@@ -182,16 +214,31 @@ public class ControlCenter {
 						+ section.getTheoreticalMaxSpeed() + "km/h");
 				// The driver modifies the speed based on the section about to enter
 				String action = vehicle.adaptSpeed(section);
+
 				double secondsThisSection = section.getLength() / (vehicle.getCurrentSpeed() * 1d / 1000 * 3600);
 				nextComment("\t" + action);
-				nextComment("\tPasa la sección en " + secondsThisSection + " segundos.");
-				totalTime += secondsThisSection;
-				nextComment("\tSu tiempo total tras el tramo " + currentSectionPosition + " es de "
-						+ timeTo2Decimals(totalTime) + " segundos");
-				currentSectionPosition++;
+				if (vehicle.getCurrentSpeed() <= section.getCurrentMaxSpeed()) {
+					nextComment("\tPasa la sección en " + secondsThisSection + " segundos.");
+					totalTime += secondsThisSection;
+					nextComment("\tSu tiempo total tras el tramo " + currentSectionPosition + " es de "
+							+ timeTo2Decimals(totalTime) + " segundos");
+					currentSectionPosition++;
+				} else {
+					// too fast!
+					nextComment("\t¡Se ha pasado de velocidad y se sale de la pista! " + vehicle.getDriver()
+							+ " queda descalificado...");
+					disqualifiedVehicles[i] = section;
+					break;
+				}
+
 			}
-			nextComment("\tFinaliza el recorrido! Su tiempo total es de " + timeTo2Decimals(totalTime) + " segundos.");
-			times[i] = totalTime;
+			if (disqualifiedVehicles[i] == null) {
+				nextComment(
+						"\tFinaliza el recorrido! Su tiempo total es de " + timeTo2Decimals(totalTime) + " segundos.");
+				times[i] = totalTime;
+			} else {
+				times[i] = Integer.MAX_VALUE;
+			}
 		}
 
 		// MODIFY THIS to show the results sorted by total time.
@@ -223,28 +270,33 @@ public class ControlCenter {
 					}
 				}
 			}
-			// Now we know the position of the next car. Also, we have to store that we
+			// Now we know the position of the next car. Disqualified cars are at the end
+			// with the max time, and we need to process them differently. Also, we have to
+			// store that we
 			// already processed it.
 			Vehicle nextVehicle = vehicles[currentShorterTimePosition];
 			Double nextVehicleTime = times[currentShorterTimePosition];
 			alreadyProcessedTime[currentShorterTimePosition] = true;
-			nextComment("\t" + nextVehicle.getDriver() + " con el número " + nextVehicle.getNumber()
-					+ " queda en posición " + (currentPosition + 1) + " con un tiempo de "
-					+ timeTo2Decimals(nextVehicleTime) + " segundos.");
+			if (disqualifiedVehicles[currentShorterTimePosition] == null) {
+				nextComment("\t" + nextVehicle.getDriver() + " con el número " + nextVehicle.getNumber()
+						+ " queda en posición " + (currentPosition + 1) + " con un tiempo de "
+						+ timeTo2Decimals(nextVehicleTime) + " segundos.");
+			} else {
+				nextComment("\t" + nextVehicle.getDriver() + " con el número " + nextVehicle.getNumber()
+				+ " quedó descalificado en " + disqualifiedVehicles[currentShorterTimePosition].getDescription());
+			}
 		}
 		// Note that we have better ways to sort this. The following code would
 		// automatically sort it. By default, TreeMap sorts all its entries according to
 		// their natural ordering
-/*		TreeMap<Double, Vehicle> positions = new TreeMap<>();
-		for (int i = 0; i < vehicles.length; i++) {
-			positions.put(times[i], vehicles[i]);
-		}
-		for (Entry<Double, Vehicle> currentVehicle: positions.entrySet()) {
-			nextComment("\t" + currentVehicle.getValue().getDriver() + " con el número " + currentVehicle.getValue().getNumber()
-			+ " tiene un tiempo de "
-			+ timeTo2Decimals(currentVehicle.getKey()) + " segundos.");
-		}
-*/
+		/*
+		 * TreeMap<Double, Vehicle> positions = new TreeMap<>(); for (int i = 0; i <
+		 * vehicles.length; i++) { positions.put(times[i], vehicles[i]); } for
+		 * (Entry<Double, Vehicle> currentVehicle: positions.entrySet()) {
+		 * nextComment("\t" + currentVehicle.getValue().getDriver() + " con el número "
+		 * + currentVehicle.getValue().getNumber() + " tiene un tiempo de " +
+		 * timeTo2Decimals(currentVehicle.getKey()) + " segundos."); }
+		 */
 	}
 
 	private static String timeTo2Decimals(double time) {
@@ -256,7 +308,8 @@ public class ControlCenter {
 		// simpleRandomRace(50, 100, 2, 5, 40, 150, 500, 2000, 70, 150);
 		// defaultRace();
 		// kartsRace();
-		kartsAndScootersRace();
+//		kartsAndScootersRace();
+		kartsAndScootersMixedRace();
 	}
 
 	/*
